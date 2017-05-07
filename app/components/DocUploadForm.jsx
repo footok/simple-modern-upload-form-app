956f052cef9f76f33c068883640b6d4ec1723e6b
Harry Lee <harry.lee@digivizer.com>
2017-05-07 17:02:56 +1000

    Add more input boxes and styles to DocUploadForm

--- app/components/DocUploadForm.jsx
+++ app/components/DocUploadForm.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 
 class DocUploadForm extends React.Component {
   constructor(props) {
@@ -6,11 +7,16 @@ class DocUploadForm extends React.Component {
     this.state = {
       data: [],
     };
+    this.handleInputChange = this.handleInputChange.bind(this);
     this.uploadFile = this.uploadFile.bind(this);
   }
 
+  handleInputChange(event) {
+    var file = event.target.files[0].name
+    document.querySelector(".upload-file").value = file
+  }
+
   uploadFile(event) {
-    alert('A name was submitted: ' + this.state.lastName);
     event.preventDefault();
   }
 
@@ -18,8 +24,47 @@ class DocUploadForm extends React.Component {
     return (
       <div className="signup-form">
         <form className="user-details" onSubmit={this.uploadFile}>
-          <input name="file" type="file" placeholder="First name" value={this.state.firstName} onChange={this.handleInputChange}/>
-          <input type="submit" />
+          <div className="lease-file">
+            <input className="upload-file" placeholder="Please find your lease.." disabled="disabled" />
+            <label>Choose a file
+            <input className="upload-button" type="file" onChange={this.handleInputChange}/>
+            </label>
+          </div>
+          <div className="driver-license-file">
+            <input className="upload-file" placeholder="Please find your driver license.." disabled="disabled" />
+            <label>Choose a file
+            <input className="upload-button" type="file" onChange={this.handleInputChange}/>
+            </label>
+          </div>
+          <div className="passport-file">
+            <input className="upload-file" placeholder="Please find your passport.." disabled="disabled" />
+            <label>Choose a file
+            <input className="upload-button" type="file" onChange={this.handleInputChange}/>
+            </label>
+          </div>
+          <div className="foreign-passport-file">
+            <input id="upload-lease" className="upload-file" placeholder="Please find your foreign passport.." disabled="disabled" />
+            <label>Choose a file
+            <input className="upload-button" type="file" onChange={this.handleInputChange}/>
+            </label>
+          </div>
+          <div className="utility-bill-file">
+            <input className="upload-file" placeholder="Please find your utility bill.." disabled="disabled" />
+            <label>Choose a file
+            <input className="upload-button" type="file" onChange={this.handleInputChange}/>
+            </label>
+          </div>
+          <div className="rent-receipt-file">
+            <input className="upload-file" placeholder="Please find your rent receipt.." disabled="disabled" />
+            <label>Choose a file
+            <input className="upload-button" type="file" onChange={this.handleInputChange}/>
+            </label>
+          </div>
+          <div className="submit-form">
+            <Link to="/complete">
+              <input type="submit" value="Submit" />
+            </Link>
+          </div>
         </form>
       </div>
     );
